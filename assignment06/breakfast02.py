0c1859f9858f2dc6c489ee8c85231d30914c96a7
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-07-19 10:33:48 +0700

    fin asm6

--- assignment06/breakfast02.py
+++ assignment06/breakfast02.py
@@ -3,12 +3,16 @@ import asyncio
 
 class Coffee:
     pass
+
 class Egg:
     pass
+
 class Bacon:
     pass
+
 class Toast:
     pass
+
 class Juice:
     pass
 
@@ -18,64 +22,64 @@ def PourCoffee():
     print(f"{time.ctime()} - Finish pour coffee...")
     return Coffee()
 
-def ApplyButter():
+async def ApplyButter():
     print(f"{time.ctime()} - Begin apply butter...")
-    time.sleep(1)
+    await asyncio.sleep(1)
     print(f"{time.ctime()} - Finish apply butter...")
 
-def FryEggs(eggs):
+async def FryEggs(eggs):
     print(f"{time.ctime()} - Begin fry eggs...")
     print(f"{time.ctime()} - Heat pan to fry eggs")
-    time.sleep(1)
+    await asyncio.sleep(1)
     for egg in range(eggs):
         print(f"{time.ctime()} - Frying", egg+1,"eggs")
-        time.sleep(1)
+        await asyncio.sleep(1)
     print(f"{time.ctime()} - Finish fry eggs...")
     print(f"{time.ctime()} - >>>>>>>>>> Fry eggs are ready...")
 
-def FryBacon():
+async def FryBacon():
     print(f"{time.ctime()} - Begin fry bacon...")
-    time.sleep(2)
+    await asyncio.sleep(2)
     print(f"{time.ctime()} - Finish fry bacon...")
     print(f"{time.ctime()} - - >>>>>>>> Fry bacon is ready...")
     return Bacon()
 
-def ToastBread(slices):
+async def ToastBread(slices):
     for slice in range(slices):
         print(f"{time.ctime()} - Toasting bread", slice + 1)
-        time.sleep(1)
+        await asyncio.sleep(1)
         print(f"{time.ctime()} - Bread", slice + 1, "toasted")
-        ApplyButter()
-        print (f"{time.ctime()} - Toast", slice + 1, "ready")
+        await ApplyButter()
+        print(f"{time.ctime()} - Toast", slice + 1, "ready")
     print(f"{time.ctime()} - >>>>>>>> Toast are ready\n")
     return Toast()
 
 def PourJuice():
-    print(f"{time.ctime()} - Begin pour joice...")
+    print(f"{time.ctime()} - Begin pour juice...")
     time.sleep(1)
-    print(f"{time.ctime()} - Finish pour joice...")
+    print(f"{time.ctime()} - Finish pour juice...")
     return Juice()
 
 async def main():
-    await PourCoffee()
+    PourCoffee()
     print(f"{time.ctime()} - >>>>>>>> Coffee is ready\n")
     
     tasks = [
-        asyncio.create_task(FryEggs(1)),
-        asyncio.create_task(FryBacon()),
-        asyncio.create_task(ToastBread(2)),
+        FryEggs(2),
+        FryBacon(),
+        ToastBread(2),
     ]
     
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+    await asyncio.gather(*tasks)
     
-    await print('\n')
-    await print(f"\n{time.ctime()} - >>>>>>>> Nealy to finished...")
-    await PourJuice()
-    
+    print('\n')
+    print(f"\n{time.ctime()} - >>>>>>>> Nearly to finished...")
+    PourJuice()
+
 ##########################################################################################################
 
 if __name__ == "__main__":
-    start_cooking = time.perf_counter() ## start time
-    main()# main function 
-    elapsed = time.perf_counter() - start_cooking ## end time
+    start_cooking = time.perf_counter()  ## start time
+    asyncio.run(main())  # main function
+    elapsed = time.perf_counter() - start_cooking  ## end time
     print(f"{time.ctime()} - Breakfast cooked in ", elapsed, "seconds.")
