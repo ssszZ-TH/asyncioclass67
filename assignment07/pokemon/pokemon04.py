862d26ef355fe1a23020c78b0cfbcb9be196acb7
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-07-26 11:04:35 +0700

    fin asm4

--- assignment07/pokemon/pokemon04.py
+++ assignment07/pokemon/pokemon04.py
@@ -3,14 +3,27 @@ import asyncio
 import json
 from pathlib import Path
 
-pokemonapi_directory = './assignment07/pokemon/pokemonapi'
-pokemonmove_directory = './assignment07/pokemon/pokemonmove'
+pokemonapi_directory = 'C:/Users/HP/Desktop/asyncioclass67/assignment07/pokemon/pokemonapi'
+pokemonmove_directory = 'C:/Users/HP/Desktop/asyncioclass67/assignment07/pokemon/pokemonmove'
 
 async def main():
-    pathlist = Path(pokemonapi_directory).glob('*.json')
-
-    # Iterate through all json files in the directory.
+    pathlist = list(Path(pokemonapi_directory).glob('*.json'))[:150]
+    
     for path in pathlist:
-        print(path)
+         # Read the contents of the json file.
+        async with aiofiles.open(path, mode='r') as f:
+            contents = await f.read()
+
+        # Load it into a dictionary and create a list of moves.
+        pokemon = json.loads(contents)
+        name = pokemon['name']
+        moves = [move['move']['name'] for move in pokemon['moves']]
+
+        move_file_path = f'{pokemonmove_directory}/{name}_moves.txt'
+         # Open a new file to write the list of moves into.
+        async with aiofiles.open(move_file_path, mode='w') as f:
+            await f.write('\n'.join(moves))
+        
+        print(f"Moves written to: {move_file_path}")
 
 asyncio.run(main())
