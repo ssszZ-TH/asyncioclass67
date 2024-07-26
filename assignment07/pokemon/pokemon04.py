2b1606f7f75e06c00228f07e39369c9f8b76e3d9
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-07-26 11:32:34 +0700

    articuno fin

--- assignment07/pokemon/pokemon04.py
+++ assignment07/pokemon/pokemon04.py
@@ -3,8 +3,8 @@ import asyncio
 import json
 from pathlib import Path
 
-pokemonapi_directory = 'C:/Users/HP/Desktop/asyncioclass67/assignment07/pokemon/pokemonapi'
-pokemonmove_directory = 'C:/Users/HP/Desktop/asyncioclass67/assignment07/pokemon/pokemonmove'
+pokemonapi_directory = './assignment07/pokemon/pokemonapi'
+pokemonmove_directory = './assignment07/pokemon/pokemonmove'
 
 async def main():
     pathlist = list(Path(pokemonapi_directory).glob('*.json'))[:150]
