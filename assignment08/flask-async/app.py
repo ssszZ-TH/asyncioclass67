a96653e61e1fda95757f7537b58af76df82b20c8
sitthipong_tufA15 <sitthipong.cham@gmail.com>
2024-08-09 11:06:42 +0700

    fin asm8

--- assignment08/flask-async/app.py
+++ assignment08/flask-async/app.py
@@ -9,13 +9,39 @@ from pypokemon.pokemon import Pokemon
 app = Flask(__name__)
 
 async def get_pokemon(client, url):
-    pass
+    print(f"{time.ctime()} - get {url}")
+    resp = await client.get(url)
+    pokemon = resp.json()
+    
+    return pokemon
 
 async def get_pokemons():
-    pass
+    async with httpx.AsyncClient() as client:
+        tasks = []
+        rand_list=[]
+        for i in range(5):
+            rand_list.append(random.randint(1,151))
+            
+        for number in rand_list:
+            url = f'https://pokeapi.co/api/v2/pokemon/{number}'
+            tasks.append(asyncio.create_task(get_pokemon(client, url)))
+        
+        pokemon_tasks = await asyncio.gather(*tasks)
+                
+        pokemon_data = []
+        for pokemon_object in pokemon_tasks:
+            pokemon_data.append(Pokemon(pokemon_object))
+
+        
+    return pokemon_data
+        
 
 @app.route('/')
 async def index():
-    pass
+    start_time = time.perf_counter()
+    pokemons = await get_pokemons()
+    end_time = time.perf_counter()
+    print(f"{time.ctime()} - Get {len(pokemons)} pokemons. Time taken: {end_time - start_time} seconds")
+    return render_template('index.html', pokemons=pokemons, end_time=end_time, start_time=start_time)
 if __name__ == '__main__':
     app.run(debug=True, port=50001)
